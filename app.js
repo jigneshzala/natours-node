4e2c8ec6aec8d95fbc8421dc3d4532c70d570462
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-03 14:34:50 +0530

    Implementing the Users Routes

--- app.js
+++ app.js
@@ -100,6 +100,42 @@ const deleteTour = (req, res) => {
     });
 
 }
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+
+    });
+}
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+
+    });
+}
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+
+    });
+}
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+
+    });
+}
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+
+    });
+}
 //Old way
 /* app.get('/api/v1/tours', getAllTours);
 app.get('/api/v1/tours/:id', getTour);
@@ -111,6 +147,9 @@ app.delete('/api/v1/tours/:id', deleteTour); */
 //New Way
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 // 4) START SERVER
 const port = 3000;
