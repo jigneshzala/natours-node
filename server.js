45220678f3099ab3db591c93694ad33c04ea1264
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-04 21:50:07 +0530

    DB connection with mongoose

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({
     path: './config.env'
 });
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB Connection successfull!'));
 
 const port = process.env.PORT || 3000;
 
