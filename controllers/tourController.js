e27a75345197ebeba3e6dd67fc3efbdbc668d525
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-06 10:57:02 +0530

    Aggregation Pipeline Matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModels');
+const APIFeatures = require('./../utils/apiFeatures');
 
 //const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -12,69 +13,11 @@ exports.aliasTopTours = (req, res, next) => {
 exports.getAllTours = async (req, res) => {
 
     try {
-        // BUILD QUERY
-        // 1A) Filtering 
-        const queryObj = {
-            ...req.query
-        };
-
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        // 1B) ADVANCE FILTERING
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-
-        // {difficulty :'easy',duration:{$gte:5}}
-        // {difficulty :'easy',duration:{gte:5}}
-        // gte, gt, ,lte, lt
-
-        //GET ALL TOURS
-        // const tours = await Tour.find();
-
-        //METHOD-1 FILTER TOURS
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //METHOD-2 FILTER TOURS
-        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-
-        // 2) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-            // sort('price ratingsAverage')
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // 3) Fields limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-
-        } else {
-            // -(minus) will exclude
-            query = query.select('-__v');
-        }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        //page=2&limit=10
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('This page does not exist');
-
-        }
 
         // EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+
+        const tours = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({
@@ -186,4 +129,68 @@ exports.deleteTour = async (req, res) => {
     }
 
 
+}
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([{
+                $match: {
+                    ratingsAverage: {
+                        $gte: 4.5
+                    }
+                }
+            },
+            {
+                $group: {
+                    // It will group result by given field in id
+                    _id: {
+                        $toUpper: '$difficulty'
+                    },
+                    numTours: {
+                        $sum: 1
+                    },
+                    numRatings: {
+                        $sum: '$ratingsQuantity'
+                    },
+                    avgRating: {
+                        $avg: '$ratingsAverage'
+                    },
+                    avgPrice: {
+                        $avg: '$price'
+                    },
+                    minPrice: {
+                        $min: '$price'
+                    },
+                    maxPrice: {
+                        $max: '$price'
+                    }
+
+                }
+            }, {
+                $sort: {
+                    avgPrice: 1
+                }
+            },
+            /*  {
+                 $match: {
+                     _id: {
+                         $ne: 'EASY'
+                     }
+                 }
+             } */
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
