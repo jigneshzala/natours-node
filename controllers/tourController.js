983a16e740242ab940171377b9c42d95b5be1a4e
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-06 13:02:12 +0530

    use built-in validators for name,difficulty and ratingsAverage

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,7 +80,7 @@ exports.createTour = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: 'fail',
-            message: 'Invalid data sent!'
+            message: err
         })
     }
 
--- models/tourModels.js
+++ models/tourModels.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 characters']
     },
     slug: String,
     duration: {
@@ -29,8 +31,7 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0'],
-        set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
+        max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
