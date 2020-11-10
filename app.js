be99badf0cb2fda1c11a6448d5efe039212d11b0
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-10 12:13:42 +0530

    Setting Security HTTP Headers using HELMET

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -22,7 +27,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({
+    limit: '10kb'
+}));
 
 // Custom Middleware
 /* app.use((req, res, next) => {
