aea92024b0f24a58339d77c16c0831c98c53df61
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-27 21:16:40 +0530

    Building Handler Factory Functions Delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 //const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -92,7 +93,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
+exports.deleteTour = factory.deleteOne(Tour);
+
+/* exports.deleteTour = catchAsync(async (req, res, next) => {
 
     const tour = await Tour.findByIdAndDelete(req.params.id);
 
@@ -105,7 +108,7 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
         data: null
     });
 
-});
+}); */
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
 
