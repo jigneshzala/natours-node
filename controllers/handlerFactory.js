6d4aea4395015401daec16f1442d6a1359a51384
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-03 21:55:37 +0530

    Building the Tour Overview

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -87,7 +87,7 @@ exports.getAll = Model => catchAsync(async (req, res, next) => {
     const features = new APIFeatures(Model.find(filter), req.query).filter().sort().limitFields().paginate();
 
     //const doc = await features.query.explain();
-    const doc = await features.query();
+    const doc = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
