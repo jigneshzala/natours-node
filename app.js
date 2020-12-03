21829dd4ab35741453ea50751566a95f98d008a0
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-03 21:06:32 +0530

    Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -80,9 +80,23 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-    res.status(200).render('base',{
-        tour:'The Forest hiker',
-        user:'Jignesh'
+    res.status(200).render('base', {
+        tour: 'The Forest hiker',
+        user: 'Jignesh'
+    });
+});
+
+app.get('/overview', (req, res) => {
+
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    });
+});
+
+app.get('/tour', (req, res) => {
+
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker TOur'
     });
 });
 
