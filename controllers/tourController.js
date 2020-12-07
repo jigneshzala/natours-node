cc4b4a4b85b1da31205c2014ed27b13de1c45d26
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-07 22:04:39 +0530

    Uploading and Processing  Multiple Images Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,74 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 //const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not an image! Please upload only images.', 400), false);
+    }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([{
+        name: 'imageCover',
+        maxCount: 1
+    },
+    {
+        name: 'images',
+        maxCount: 3
+    }
+]);
+
+// upload.single('image') req.file
+// upload.array('images', 5) req.files
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+    if (!req.files.imageCover || !req.files.images) return next();
+
+    // 1) Cover image
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+    await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({
+            quality: 90
+        })
+        .toFile(`public/img/tours/${req.body.imageCover}`);
+
+    // 2) Images
+    req.body.images = [];
+
+    await Promise.all(
+        req.files.images.map(async (file, i) => {
+            const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+            await sharp(file.buffer)
+                .resize(2000, 1333)
+                .toFormat('jpeg')
+                .jpeg({
+                    quality: 90
+                })
+                .toFile(`public/img/tours/${filename}`);
+
+            req.body.images.push(filename);
+        })
+    );
+
+    next();
+});
 
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -175,43 +240,45 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
 });
 
 exports.getDistances = catchAsync(async (req, res, next) => {
-    const { latlng, unit } = req.params;
+    const {
+        latlng,
+        unit
+    } = req.params;
     const [lat, lng] = latlng.split(',');
-  
+
     const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
-  
+
     if (!lat || !lng) {
-      next(
-        new AppError(
-          'Please provide latitutr and longitude in the format lat,lng.',
-          400
-        )
-      );
+        next(
+            new AppError(
+                'Please provide latitutr and longitude in the format lat,lng.',
+                400
+            )
+        );
     }
-  
-    const distances = await Tour.aggregate([
-      {
-        $geoNear: {
-          near: {
-            type: 'Point',
-            coordinates: [lng * 1, lat * 1]
-          },
-          distanceField: 'distance',
-          distanceMultiplier: multiplier
+
+    const distances = await Tour.aggregate([{
+            $geoNear: {
+                near: {
+                    type: 'Point',
+                    coordinates: [lng * 1, lat * 1]
+                },
+                distanceField: 'distance',
+                distanceMultiplier: multiplier
+            }
+        },
+        {
+            $project: {
+                distance: 1,
+                name: 1
+            }
         }
-      },
-      {
-        $project: {
-          distance: 1,
-          name: 1
-        }
-      }
     ]);
-  
+
     res.status(200).json({
-      status: 'success',
-      data: {
-        data: distances
-      }
+        status: 'success',
+        data: {
+            data: distances
+        }
     });
-  });
+});
