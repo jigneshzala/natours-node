2e02d295ab9bce3a412f544bdb175042edaa8dc7
jignesh zala <jigneshkumar1494@gmail.com>
2020-12-08 21:54:30 +0530

    Processing Payments on the Front-End

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,10 @@ import {
 import {
     updateSettings
 } from './updateSettings';
+import {
+    bookTour
+} from './stripe';
+
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -17,6 +21,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -68,4 +73,13 @@ if (userPasswordForm)
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
+    });
+
+if (bookBtn)
+    bookBtn.addEventListener('click', e => {
+        e.target.textContent = 'Processing...';
+        const {
+            tourId
+        } = e.target.dataset;
+        bookTour(tourId);
     });
